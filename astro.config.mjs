82164adc63415c7cf2aa15ad90a03e06db3adf7d
Ernesto1207 <ernestosolanoballena@hotmail.com>
2024-07-27 09:15:46 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://ernesto1207.github.io',
-  // base: 'Restaurant-Monsefu',
+  site: 'https://ernesto1207.github.io',
+  base: 'Restaurant-Monsefu',
   output: 'server',
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: vercel()
 });
