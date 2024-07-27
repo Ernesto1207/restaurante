f41d1c134fe139789f662d4eb26bcd0b6ac758a4
Ernesto1207 <ernestosolanoballena@hotmail.com>
2024-07-27 08:58:38 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ernesto1207.github.io',
+  base: 'Restaurant-Monsefu',
   output: 'server',
   integrations: [tailwind(), svelte()]
 });
