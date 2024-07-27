9b294cd9ce54569af946684023e7137bfc4152ec
Ernesto1207 <ernestosolanoballena@hotmail.com>
2024-07-27 09:24:36 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ernesto1207.github.io',
-  base: 'Restaurant-Monsefu',
+
   output: 'server',
   integrations: [tailwind(), svelte()],
   adapter: vercel()
